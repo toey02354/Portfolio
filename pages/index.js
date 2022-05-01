1338f8863439dbf900b1d22d3899cb9c2da83f92
toey02354 <nattapoln02354@gmail.com>
2022-05-01 20:31:26 +0700

    init

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,38 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Suspense } from "react";
 
-export default function Home() {
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { Canvas } from "@react-three/fiber";
+import { MeshDistortMaterial, OrbitControls } from "@react-three/drei";
+// import justaGirl from "../components/Justagirl";
+import Iphone from "../components/Iphone";
+import Justagirl from "../components/Justagirl";
+
+const Home = () => {
+  // const justagirl = useLoader(GLTFLoader, "/justagirl.gltf");
+  const Earth = () => {
+    return (
+      <mesh position={[0, 0, 0]} scale={2}>
+        <sphereGeometry attach="geometry" args={[1, 30, 30]} />
+        <MeshDistortMaterial
+          attach="material"
+          color="#000354"
+          speed={2}
+          distort={0.5}
+        />
+        <OrbitControls enableZoom={false} />
+      </mesh>
+    );
+  };
+  const IPhone2 = () => {
+    return (
+      <mesh position={[0, -1, 0]} scale={0.03}>
+        <Justagirl />
+        <OrbitControls enableZoom={false} />
+      </mesh>
+    );
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +42,21 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <h1>Three Js</h1>
+        <div className={styles.canvas}>
+          <Canvas>
+            <Suspense fallback={null}>
+              <IPhone2 />
+              {/* <justaGirl /> */}
+              {/* <Earth /> */}
+            </Suspense>
+            <ambientLight />
+            <pointLight position={[1, 1, 5]} intensity={2} />
+          </Canvas>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
