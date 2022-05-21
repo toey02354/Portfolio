2aa63311c731e1d019b7cc434af1f0b358a26212
toey02354 <nattapoln02354@gmail.com>
2022-05-21 23:33:32 +0700

    fix bug

--- pages/test.jsx
+++ pages/test.jsx
@@ -1,13 +1,7 @@
-import React, { useState, useRef } from "react";
-import TypingEffect, { startTyping } from "../components/TypingEffect";
+import React from "react";
+import TypingEffect from "../components/TypingEffect";
 
 const test = () => {
-  const typingRef = useRef();
-  const handleClick = () => {
-    console.log(typingRef.current);
-    typingRef.current.append("hello");
-  };
-
   return (
     <div className="h-screen bg-green-200 flex flex-col item-center">
       {/* <div className="bg-red-500 h-[100px] w-[100px]" />
