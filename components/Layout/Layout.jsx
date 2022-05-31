1a69b49d17b2f59ac18eedb386b2abbccf2f3f6a
natthaphol_lsc <natthaphol@looksocial.asia>
2022-05-31 13:19:09 +0700

    scroll button

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../Navbar";
-import Footer from "../Footer/Footer";
+import Footer from "../Footer";
+import ArrowCircleUpOutlinedIcon from "@mui/icons-material/ArrowCircleUpOutlined";
 
 const Layout = ({ children }) => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const handleTop = () => {
+    window.scrollTo({ behavior: "smooth", top: 0 });
+  };
+
+  useEffect(() => {
+    setPosition({ x: pageXOffset, y: pageYOffset });
+  }, [position]);
+
   return (
     <div className="">
       <Navbar />
       {children}
       <Footer />
+      <button
+        className={`TW-DOMINANT-BG fixed bottom-0 right-0 m-4 p-2 rounded-full text-white ${
+          position.y <= 300 ? "hidden" : null
+        }`}
+        onClick={handleTop}
+      >
+        <ArrowCircleUpOutlinedIcon />
+      </button>
     </div>
   );
 };
