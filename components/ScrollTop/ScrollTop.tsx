d1c5f7e4ab0ff1179713e5dca039d89091f795dd
toey02354 <nattapoln02354@gmail.com>
2022-05-31 12:37:01 +0700

    scroll button

---
+++ components/ScrollTop/ScrollTop.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button, makeStyles } from "@mui/material";
+
+const styles = makeStyles({
+  scrollTopBtn: {
+    backgroundColor: "red",
+  },
+});
+
+const ScrollTop = () => {
+  return <div>ScrollTop</div>;
+};
+
+export default ScrollTop;
