b078e0a98de584159812a3b72e52042671016607
toey02354 <nattapoln02354@gmail.com>
2022-06-07 20:33:32 +0700

    cursor pointer

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -55,14 +55,14 @@ const Navbar = () => {
       </div>
       <div className="flex bg-slate-600 rounded-xl">
         <div
-          className={`p-2 `.concat(
+          className={`p-2 cursor-pointer `.concat(
             !dark ? "TW-HIGHLIGHT-TEXT bg-slate-700 rounded-xl" : null
           )}
         >
           <LightMode onClick={() => setDark(false)} />
         </div>
         <div
-          className={`p-2 `.concat(
+          className={`p-2 cursor-pointer `.concat(
             dark ? "TW-HIGHLIGHT-TEXT bg-slate-700 rounded-xl" : null
           )}
         >
