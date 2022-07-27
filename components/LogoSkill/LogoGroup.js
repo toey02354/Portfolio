715027993ab1adc22c03dc7fc62d8384ad4adb19
toey02354 <nattapoln02354@gmail.com>
2022-07-28 00:28:50 +0700

    updated logo skill

--- components/LogoSkill/LogoGroup.js
+++ components/LogoSkill/LogoGroup.js
@@ -1,18 +1,23 @@
+import logoangular from "../../public/Images/logo-angular.png";
 import logodjango from "../../public/Images/logo-django.png";
 import logodocker from "../../public/Images/logo-docker.png";
 import logoexpressjs from "../../public/Images/logo-expressjs.png";
 import logofastapi from "../../public/Images/logo-fastapi.png";
 import logogitlab from "../../public/Images/logo-gitlab.png";
+import logoionic from "../../public/Images/logo-ionic.png";
 import logojs from "../../public/Images/logo-js.png";
 import logomongodb from "../../public/Images/logo-mongodb.png";
 import logomysql from "../../public/Images/logo-mysql.png";
+import logomui from "../../public/Images/logo-mui.png";
 import logonextjs from "../../public/Images/logo-nextjs.png";
+import logonestjs from "../../public/Images/logo-nestjs.svg";
 import logonodejs from "../../public/Images/logo-nodejs.png";
 import logopostgres from "../../public/Images/logo-postgres.png";
 import logopython from "../../public/Images/logo-python.png";
 import logoreactjs from "../../public/Images/logo-reactjs.png";
 import logoredux from "../../public/Images/logo-redux.png";
 import logoredis from "../../public/Images/logo-redis.png";
+import logosvelte from "../../public/Images/logo-svelte.png";
 import logotailwind from "../../public/Images/logo-tailwindcss.svg";
 import logolinux from "../../public/Images/logo-linux.png";
 import logots from "../../public/Images/logo-ts.png";
@@ -23,21 +28,21 @@ export const logoGroup1 = [
   logonextjs,
   logoreactjs,
   logoredux,
-  logotailwind,
+  logomui,
 ];
 export const logoGroup2 = [
+  logoangular,
+  logosvelte,
+  logoionic,
   logonodejs,
+  logonestjs,
   logoexpressjs,
+];
+export const logoGroup3 = [
   logopython,
   logodjango,
   logofastapi,
-  logolinux,
-];
-export const logoGroup3 = [
-  logogitlab,
-  logodocker,
   logopostgres,
-  logomysql,
   logomongodb,
-  logoredis,
+  logodocker,
 ];
