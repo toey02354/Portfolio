241d473236d1c396d22b606de49d3b7842a28347
toey02354 <natapoln02354@gmail.com>
2022-12-01 17:25:55 +0700

    update logic

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -8,19 +8,23 @@ import { MdDarkMode, MdLightMode } from "react-icons/md";
 const Navbar = () => {
   const router = useRouter();
   const { dark, setDark } = useTheme();
+  
+  const changeTheme = () => {
+    setDark(currTheme => !currTheme)
+  }
 
   return (
     <div className="bg-main flex justify-between items-center py-4 px-6 text-xl text-white">
       <div className="flex items-center gap-8">
         <div className="hidden lg:flex lg:justify-center lg:items-center p-3">
-          <strong>TOEY</strong> <text>Portfolio</text>
+          <strong>TOEY</strong> <div>Portfolio</div>
         </div>
         <div className="flex gap-4">
           {NavItems.map((item) => (
             <Link href={item.path} key={item.name} passHref>
               <button
                 className={`flex items-center gap-1 hover:text-amber-500 ${
-                  router.pathname == item.path ? "text-highlight" : null
+                  router.pathname == item.path ? "text-highlight" : undefined 
                 }`}
               >
                 {item.icon} <div className="hidden xl:block">{item.name}</div>
@@ -31,7 +35,7 @@ const Navbar = () => {
       </div>
       <div
         className="flex p-2 bg-slate-600 rounded-xl cursor-pointer text-highlight"
-        onClick={() => setDark((currVal) => !currVal)}
+        onClick={changeTheme}
       >
         {dark ? <MdDarkMode /> : <MdLightMode />}
       </div>
